86a79fe4e1744f27d19e12d3f8ea1c1cf52b331a
unknown <myoungin729@gmail.com>
2024-12-19 10:26:01 +0900

    day9 solved - dirty

--- src/answers/d9.rs
+++ src/answers/d9.rs
@@ -45,4 +45,66 @@ pub fn d9_1(input: String) {
 }
 
 pub fn d9_2(input: String) {
+    let mut v: Vec<(i32, i32)> = Vec::new();
+    let mut is_num = 1;
+    let mut id = 0;
+    for c in input.chars() {
+        let n = c as i32 - '0' as i32;
+        if is_num == 1 {
+            v.push((id, n));
+            id += 1;
+        } else {
+            v.push((-1, n));
+        }
+        is_num = 1 - is_num;
+    }
+    // println!("{:?}", v);
+    id -= 1;
+    let mut start = 0;
+    let mut end = v.len() - 1;
+    loop {
+        if id == 0 {
+            break;
+        }
+        while end > 0 && v[end].0 != id {
+            end -= 1;
+        }
+        start = 0;
+        while start < v.len() && (v[start].0 != -1 || v[start].1 < v[end].1) {
+            start += 1;
+        }
+        // println!("{} {} {} {}", id, start, end, v.len());
+        if start >= end {
+            id -= 1;
+            continue;
+        }
+        let left = v[start].1 - v[end].1;
+        if left != 0 {
+            v[start] = (-1, left);
+            v.insert(start, v[end]);
+            v[end+1] = (-1, v[end+1].1);
+        } else {
+            v[start] = v[end];
+            v[end] = (-1, v[end].1);
+        }
+        id -= 1;
+        if id == 0 {
+            break;
+        }
+    }
+    // println!("{:?}", v);
+    let mut sum: i64 = 0;
+    let mut idx = 0;
+    for (id, num) in v {
+        if id == -1 {
+            idx += num;
+            continue;
+        }
+        for i in 0..num {
+            // println!("{} {}", idx+i, id);
+            sum += (idx as i64 + i as i64) * id as i64; 
+        }
+        idx += num;
+    }
+    println!("{}", sum);
 }
